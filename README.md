78023868cc1b4b3a430c0919480f1a27e7387f3a
匡一珉 <kuangyimin@xdf.cn>
2018-11-28 18:10:03 +0800

    list

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # main language: python
+# maybe javascript
---
+++ reverse_linked_list.py
@@ -0,0 +1,6 @@
+# reverse linked list
+def reverseList(self, head):
+    cur, prev = head, None
+    while cur:
+        cur.next, prev, cur = prev, cur, cur.next
+    return prev
